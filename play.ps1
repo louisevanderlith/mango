c2788405bdeec81ba59681b03b09c9124f7e29ec
Louise van der Lith <louisevanderlith@gmail.com>
2018-01-08 17:07:12 +0200

    Updated play.ps1/.json so we can run custom commands for every application. Applications are now run from their own directories and NOT the bin folder.

--- play.ps1
+++ play.ps1
@@ -3,20 +3,23 @@ function startPlay() {
     $wd = Convert-Path .
 
     Foreach($prog in $progs){
-        $progPath = $wd + "/" + $prog.path
-        if(Test-Path($progPath)){
-            $cmd = "Write-Host 'Starting' $prog -ForegroundColor 'red'; cd $progPath; $prog.cmd; Read-Host"
+        $progName = $prog.name
+        $progCmd = $prog.cmd
+        $progPath = $wd + "\\" + $prog.type + "\\" + $progName
 
-            if($prog -eq 'gate'){
+        if(Test-Path($progPath)){
+
+            if($progName -eq 'gate'){
                 Write-Host 'Gate must wait for all applications to register with the router before starting.' -ForegroundColor "green"
-                Start-Sleep -s 2
+                Start-Sleep -s 4
             }
 
+            $cmd = "Write-Host 'Starting' $progName -ForegroundColor 'red'; cd $progPath; $progCmd; Read-Host"
             Start-Process powershell -argument $cmd
 
-            if($prog -eq 'router'){
+            if($progName -eq 'router'){
                 Write-Host 'Giving router some time to spin up.' -ForegroundColor "green"
-                Start-Sleep -s 1
+                Start-Sleep -s 2
             }
         } else {
             Write-Host 'Directory' $progPath 'not Found. Please ensure "./build" has been run.' -ForegroundColor "red"
