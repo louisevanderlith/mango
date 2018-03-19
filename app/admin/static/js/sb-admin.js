7d95e2c4d0543de94f9fdee0118e30bd31a15e79
Vicky <vicky@payteq.com>
2018-03-19 20:09:24 +0200

    Tab changes

--- app/admin/static/js/sb-admin.js
+++ app/admin/static/js/sb-admin.js
@@ -1,9 +1,5 @@
 (function($) {
   "use strict"; // Start of use strict
-  // Configure tooltips for collapsed side navigation
-  $('.navbar-sidenav [data-toggle="tooltip"]').tooltip({
-    template: '<div class="tooltip navbar-sidenav-tooltip" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>'
-  })
   // Toggle the side navigation
   $("#sidenavToggler").click(function(e) {
     e.preventDefault();
