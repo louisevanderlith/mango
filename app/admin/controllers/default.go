9768279ecdc19e4df8c3e630e021eeff3af8f156
Vicky <vicky@payteq.com>
2018-07-15 18:14:15 +0200

    master page - side menu

--- app/admin/controllers/default.go
+++ app/admin/controllers/default.go
@@ -1,11 +1,16 @@
 package controllers
 
-import "github.com/louisevanderlith/mango/app/admin/logic"
+import (
+	"github.com/louisevanderlith/mango/app/admin/logic"
+	"github.com/louisevanderlith/mango/util/control"
+)
 
 type DefaultController struct {
-	logic.MenuController
+	control.UIController
 }
 
 func (c *DefaultController) Get() {
 	c.Setup("default")
+	c.CreateSideMenu(logic.GetMenu("/"))
+
 }
