d159e4956b367298ec212c8bc8c4705436be8ee2
Louise van der Lith <louisevanderlith@gmail.com>
2019-01-21 19:23:39 +0200

    Moved code to separate repositories.

--- pkg/service.go
+++ pkg/service.go
@@ -1,4 +1,4 @@
-package util
+package mango
 
 import (
 	"bytes"
@@ -8,7 +8,7 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/louisevanderlith/mango/pkg/enums"
+	"github.com/louisevanderlith/mango/enums"
 )
 
 type Service struct {
--- pkg/validation_test.go
+++ pkg/validation_test.go
@@ -1,4 +1,4 @@
-package util
+package mango
 
 import "testing"
 
