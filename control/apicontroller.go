fdbb218209a6c239227497ee596a3342bcef86ce
Louise van der Lith <louisevanderlith@gmail.com>
2019-02-24 11:04:48 +0200

    Removed unused function

--- control/apicontroller.go
+++ control/apicontroller.go
@@ -3,6 +3,7 @@ package control
 import (
 	"encoding/json"
 	"log"
+	"net/http"
 	"strconv"
 
 	"github.com/louisevanderlith/mango"
@@ -88,21 +89,4 @@ func (ctrl *APIController) GetKeyedRequest() (WithKey, error) {
 	err := json.Unmarshal(ctrl.Ctx.Input.RequestBody, &result)
 
 	return result, err
-}
-
-func GetFileContentType(firstBits []byte) (string, error) {
-
-    // Only the first 512 bytes are used to sniff the content type.
-    buffer := make([]byte, 512)
-
-    _, err := out.Read(buffer)
-    if err != nil {
-        return "", err
-    }
-
-    // Use the net/http package's handy DectectContentType function. Always returns a valid 
-    // content-type by returning "application/octet-stream" if no others seemed to match.
-    contentType := 
-
-    return contentType, nil
 }
